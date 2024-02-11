ac81c6400cd283b5d58bc6400d45a367800315ee
ashuy <ashuyad63@gmail.com>
2024-02-11 22:59:28 +0530

    Variable declaration and initialization programm

--- Variables.java
+++ Variables.java
@@ -2,6 +2,8 @@ public class Variables {
     public static void main(String[] args) {
         int x ; // declaration ex: ek x naam ka dabba bana diya
         x = 6; //initialization us dabbe me 6 daal diya
+        int y = 8;
         System.out.println(x);
+        System.out.println(y);
     }
 }
