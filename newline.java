0ab302b3422732871982428019b8699804a010a6
ashuy <ashuyad63@gmail.com>
2024-02-11 22:30:13 +0530

    New line programm

---
+++ newline.java
@@ -0,0 +1,8 @@
+public class newline {
+    public static void main(String[] args){
+        System.out.println();
+        System.out.println("hello earth");
+        System.out.println("hello jupiter");
+        System.out.println("Hello mars");
+    }
+}
