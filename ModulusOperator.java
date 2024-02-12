b64a02f487c24963e13b44b8d57567ba14ae3c45
ashuy <ashuyad63@gmail.com>
2024-02-12 14:15:26 +0530

    Modulus Operator programm

---
+++ ModulusOperator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ModulusOperator {
+    public static void main (String[] args){
+        // modulus usually gives us the remainder
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enetr your num a :");
+        int a = sc.nextInt();
+        System.out.print("enter your num b :");
+        int b = sc.nextInt();
+        int c = a%b;
+        System.out.println(c);
+    }
+}
