ce49bc148c369712b8a893d7d60e7f7848ff2524
ashuy <ashuyad63@gmail.com>
2024-02-12 12:42:50 +0530

    Calculate Simple Interest programm

---
+++ SimpleInterest.java
@@ -0,0 +1,11 @@
+public class SimpleInterest {
+    public static void main(String[] args) {
+        double p = 2000;
+        double r = 3.75;
+        double t = 3.5;
+        double si = (p*r*t)/100;
+        System.out.println("the simple interest is : "+si);
+
+
+    }
+}
