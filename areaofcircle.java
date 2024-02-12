615e05baa6c13fdc36843533920396ecad7ff909
ashuy <ashuyad63@gmail.com>
2024-02-12 12:24:36 +0530

    area of a circle programm

---
+++ areaofcircle.java
@@ -0,0 +1,9 @@
+public class areaofcircle {
+    public static void main(String[] args) {
+        double pi = 3.141592;
+        double r = 3.7;
+        double a = pi*r*r;
+        System.out.println(a);
+
+    }
+}
